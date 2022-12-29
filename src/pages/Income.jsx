259f3de4a6da93a3c1facfa9d3256c22f1b3316a
aub20 <aubreygailaamante@iskolarngbayan.pup.edu.ph>
2022-12-29 22:50:31 +0800

    income and dummy js

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -7,6 +7,10 @@ import {
   Search,
   Inject,
   Toolbar,
+  Edit,
+  Selection,
+  Sort, 
+  Filter
 } from "@syncfusion/ej2-react-grids";
 
 import { employeesData, employeesGrid } from "../data/dummy";
@@ -24,15 +28,16 @@ const Income = () => {
         dataSource={employeesData}
         allowPaging
         allowSorting
-        toolbar={["Search"]}
+        toolbar={["Search","Delete"]}
         width="auto"
+        editSettings={{ allowDeleting: true, allowEditing: true }}
       >
         <ColumnsDirective>
           {employeesGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[Page, Search, Toolbar]} />
+        <Inject services={[Page, Search, Toolbar,Edit, Selection, Sort, Filter]} />
       </GridComponent>
     </div>
   );
