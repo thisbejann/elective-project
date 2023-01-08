070631c6a2d1f733adb7e43735662f70bd4a74c6
vincevergel <vinceallen.vergel@gmail.com>
2023-01-08 23:47:44 +0800

    All goods income

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -1,59 +1,90 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   GridComponent,
   ColumnsDirective,
   ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
   Page,
-  Search,
-  Inject,
-  Toolbar,
+  ExcelExport,
+  PdfExport,
   Edit,
-  Selection,
-  Sort, 
-  Filter
+  Inject,
+  Search,
 } from "@syncfusion/ej2-react-grids";
 
-import { employeesData, incomeGrid } from "../data/dummy";
+import { employeesData, contextMenuItems, incomeGrid } from "../data/dummy";
 import { Header, IncomeDialog } from "../components";
 
-import { auth } from "../utils/firebase";
+import { auth, db } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { documenteditor } from "@syncfusion/ej2";
+
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Income = () => {
   const navigate = useNavigate();
   const [user, loading] = useAuthState(auth);
+  const [userIncome, setUserIncome] = useState([]);
+  const { isClicked } = useStateContext();
 
   const getData = async () => {
     if (loading) return;
     if (!user) return navigate("/auth/login");
+
+    const collectionRef = collection(db, "income");
+    const q = query(collectionRef, where("user", "==", user.uid));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setUserIncome(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    return unsubscribe;
   };
 
+  console.log(userIncome);
   useEffect(() => {
     getData();
   }, [user, loading]);
 
+  console.log(isClicked);
+
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div
+      className={`m-2 mt-10 md:m-10 p-2 md:p-10 bg-white rounded-3xl ${
+        isClicked ? "hidden sm:block" : ""
+      }`}
+    >
       <div className="flex justify-between mt-12 md:mt-3">
         <Header category="Page" title="Income" />
         <IncomeDialog />
       </div>
 
       <GridComponent
-        dataSource={employeesData}
+        id="gridcomp"
+        // datasource are the incomes that are fetched from the database and stored in the userExpenses state and then mapped to the grid. only the amountValue, dateValue, categoryValue, descriptionValue, and transactionValue are mapped to the grid.
+        dataSource={userIncome.map((item) => ({
+          amountValue: item.income.amountValue,
+          dateValue: item.income.dateValue,
+          categoryValue: item.income.categoryValue,
+          descriptionValue: item.income.descriptionValue,
+          transactionValue: item.income.transactionValue,
+        }))}
         allowPaging
         allowSorting
-        toolbar={["Search","Delete"]}
-        width="auto"
-        editSettings={{ allowDeleting: true, allowEditing: true }}
+        editSettings={{ allowDeleting: true }}
+        toolbar={["Search", "Delete"]}
       >
         <ColumnsDirective>
           {incomeGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[Page, Search, Toolbar,Edit, Selection, Sort, Filter]} />
+        <Inject 
+          services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Search]} 
+        />
       </GridComponent>
     </div>
   );
