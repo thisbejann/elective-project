8342c26a7444c1e4eaa8a2c4b3983aea9db0467e
Jann Jaspher Santos <75710250+thisbejann@users.noreply.github.com>
2023-06-06 23:38:03 +0800

    team page changes

--- src/components/TeamProfile.jsx
+++ src/components/TeamProfile.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const TeamProfile = ({ name, role, imageSrc, description }) => {
   return (
-    <div className="flex flex-col items-center justify-center py-5 lg:py-0">
+    <div className="flex flex-col items-center justify-center rounded-xl bg-gray-200 p-5 shadow-sm dark:bg-main-dark-bg">
       <img className="h-40 w-40 rounded-full object-cover" src={imageSrc} alt="profile" />
       <p className="mt-2 text-lg font-semibold dark:text-white">{name}</p>
       <div className="flex flex-col items-center justify-center gap-2 px-3">
         <p className="text-sm text-gray-800 dark:text-white">{role}</p>
-        {/* dummy description */}
         <p className="text-justify text-sm text-gray-800 dark:text-slate-300">{description}</p>
       </div>
     </div>
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -13,20 +13,20 @@ const Team = () => {
     >
       <div className="mt-12 flex flex-col justify-between md:mt-3 ">
         <Header category="Page" title="Team" />
-        <div className="flex items-center justify-center rounded-3xl bg-white px-5 dark:bg-secondary-dark-bg md:m-10  md:p-10 lg:mt-[5rem] lg:px-[8rem]">
+        <div className="flex items-center justify-center px-5 dark:bg-secondary-dark-bg md:m-10  md:p-10 lg:mt-[5rem] lg:px-[8rem]">
           {/* container for 3 column our team photo with description */}
-          <div className="flex w-full flex-col justify-evenly gap-5 lg:flex-row">
+          <div className="flex w-full flex-col gap-5 lg:flex-row">
             <TeamProfile
               name="Aubrey Gail"
               imageSrc="images/AMANTE, AUBREY GAIL A,.jpg"
               role="Project Manager"
-              description="I'm Aubrey Gail A. Amante from Marikina City. A Computer Engineering student from Polytechnic University of the Philippines. I am utilizing my good analytical thinking and problem-solving abilities to create innovative solutions in computer engineering, tackling challenging problems in software development and system optimization."
+              description="I'm Aubrey Gail A. Amante from Marikina City. A Computer Engineering student from Polytechnic University of the Philippines. I am utilizing my good analytical thinking and problem-solving abilities to create innovative solutions in computer engineering, tackling challenging problems in software development and system optimization."
             />
             <TeamProfile
               name="Jann Jaspher"
               imageSrc="images/SANTOS, JANN JASPHER V..jpg"
               role="Frontend Developer"
-              description="Jann Jaspher is a passionate front-end web developer with expertise in HTML, CSS, and JavaScript. They excel at creating visually stunning user interfaces and collaborating with designers and back-end developers. With a keen eye for design and a commitment to delivering high-quality code, they are able to create dynamic, interactive, and user-friendly websites."
+              description="Jann Jaspher is a passionate front-end web developer with expertise in HTML, CSS, and JavaScript. I excel at creating visually stunning user interfaces and collaborating with designers and back-end developers. With a keen eye for design and a commitment to delivering high-quality code, I am able to create dynamic, interactive, and user-friendly websites."
             />
             <TeamProfile
               name="Vince Allen"
